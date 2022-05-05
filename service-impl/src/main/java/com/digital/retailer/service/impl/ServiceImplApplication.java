10dbdc2c94a46f29f46a53059e4b13a943f972c1
Ajay <kalathilajay.menon@mrcooper.com>
2022-05-04 20:54:01 -0500

    base-skeloton

---
+++ service-impl/src/main/java/com/digital/retailer/service/impl/ServiceImplApplication.java
@@ -0,0 +1,13 @@
+package com.digital.retailer.service.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceImplApplication.class, args);
+    }
+
+}
