405096b33be6d8477e08e9b76e749b2a43a77451
Ajay <kalathilajay.menon@mrcooper.com>
2022-05-04 22:03:29 -0500

    core implementation + code cleanup

--- service-impl/src/test/java/com/digital/retailer/service/impl/ServiceImplApplicationTests.java
+++ service-impl/src/test/java/com/digital/retailer/service/impl/ServiceImplApplicationTests.java
@@ -1,13 +1,34 @@
 package com.digital.retailer.service.impl;
 
+import com.digital.retailer.service.impl.service.PointsServiceManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class ServiceImplApplicationTests {
 
+    @Autowired
+    PointsServiceManager pointsServiceManager;
+
     @Test
-    void contextLoads() {
+    void whenValidTransaction_ThenReturnPointsGiven() {
+        var transactions = Flux.just (10.00, 75.00, 101.00, 102.00, 103.00);
+        StepVerifier.create(pointsServiceManager.calculatePoints(transactions))
+                .expectNext(187l) //total sum (75-50) + (50 + (101-101)) + (50 + (101-102)) + (50 + (101-103))
+                .expectNextCount(0) //total sum should be return above and after that nothing should be expected
+                .verifyComplete();
+    }
+
+    @Test
+    void whenInValidTransaction_ThenReturnPointsGiven() {
+        var transactions = Flux.just (10.00, 75.00, 101.00, 102.00, 103.00, Double.NaN, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+        StepVerifier.create(pointsServiceManager.calculatePoints(transactions))
+                .expectNext(187l) //total sum (75-50) + (50 + (101-101)) + (50 + (101-102)) + (50 + (101-103))
+                .expectNextCount(0) //total sum should be return above and after that nothing should be expected
+                .verifyComplete();
     }
 
 }
