adcfe05dab3bfcd0688040c8ad7e7b7eb1083e91
kamenon2 <kamenon2@illinois.edu>
2022-05-21 11:41:17 -0500

    removed unused properties

--- service-impl/src/main/java/com/digital/retailer/services/impl/controller/LegacyController.java
+++ service-impl/src/main/java/com/digital/retailer/services/impl/controller/LegacyController.java
@@ -1,2 +1,46 @@
-package com.digital.retailer.services.impl.controller;public class LegacyController {
+package com.digital.retailer.services.impl.controller;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Data
+class Person {
+    long customerId;
+    String name;
+    LocalDate dateOfBirth;
+}
+
+@RestController
+public class LegacyController {
+
+    @GetMapping( value = "/customers/{customer-id}/details")
+    public List<Person> get( @PathVariable (value = "customer-id") Long customerid) {
+        List<Person> persons = new ArrayList<>();
+
+        Person person1 = new Person();
+        person1.setCustomerId(10l);
+        person1.setName("Ajay1");
+        person1.setDateOfBirth(LocalDate.now());
+
+        Person person2 = new Person();
+        person1.setCustomerId(100l);
+        person1.setName("Ajay2");
+        person1.setDateOfBirth(LocalDate.now());
+
+        persons.add(person1);
+        persons.add(person2);
+
+        persons.forEach(System.out::println);
+        persons.sort(Comparator.comparing(Person::getDateOfBirth));
+        persons.forEach(System.out::println);
+
+        return persons;
+    }
 }
--- service-impl/src/main/java/com/digital/retailer/services/impl/exception/GlobalExceptionHandler.java
+++ service-impl/src/main/java/com/digital/retailer/services/impl/exception/GlobalExceptionHandler.java
@@ -2,15 +2,12 @@ package com.digital.retailer.services.impl.exception;
 
 import com.digital.retailer.services.impl.controller.DigitalRetailerController;
 import com.digital.retailer.services.openapi.api.CustomersApi;
-import com.digital.retailer.services.openapi.model.Error;
 import com.digital.retailer.services.openapi.model.Errors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 @RestControllerAdvice(basePackageClasses = {CustomersApi.class, DigitalRetailerController.class})
